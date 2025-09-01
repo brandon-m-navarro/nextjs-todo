b58cbe9da765c6ce6f8482c6c276d6f68f5e8d2f
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-09-01 12:34:10 -0400

    connecting postgres db

--- app/ui/edit-task-form.tsx
+++ app/ui/edit-task-form.tsx
@@ -13,7 +13,7 @@ import { Button } from "./button";
 import { useActionState } from "react";
 // import { authenticate } from '@/app/lib/actions';
 import SimpleAnimation from "./animation";
-import RubiksCube from "./rubiks_cube";
+
 
 export default function EditTaskForm() {
   // const [errorMessage, formAction, isPending] = useActionState(
@@ -46,8 +46,6 @@ export default function EditTaskForm() {
     >
 
       <SimpleAnimation/>
-
-      <RubiksCube />
 
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8 w-[28rem]">
         <h1 className={`${manrope.className} text-[36px] ml-[6px] mb-[12px] mt-[0px]`}>Add Task</h1>
