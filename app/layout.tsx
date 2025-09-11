ca4f35cbbd2d3633d1c15aef5221afdf15cab1f2
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-09-11 17:59:15 -0400

    Production build SUCCESS! Added back in padding and white background to new root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased min-h-screen`}>{children}</body>
+      <body className={`antialiased min-h-screen flex-1 p-6 bg-white!`}>{children}</body>
     </html>
   );
 }
