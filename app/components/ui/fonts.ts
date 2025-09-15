6497cb07ecdda61cb4af153a7620ab8cd2dd04cd
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-09-15 17:07:42 -0400

    Updated next.config and fonts.ts to prevent issues loading custom GoogleFonts

--- app/components/ui/fonts.ts
+++ app/components/ui/fonts.ts
@@ -2,4 +2,8 @@ import { Inter, Manrope } from 'next/font/google';
 
  
 export const inter = Inter({ subsets: ['latin'] });
-export const manrope = Manrope({ weight: ['400', '700'], subsets: ['latin'] });
+export const manrope = Manrope({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    display: 'swap'
+});
