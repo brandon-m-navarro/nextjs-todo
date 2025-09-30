948b13f0c3283d7c5823630b0dc089cdef2f999e
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-09-30 15:59:10 -0400

    Slight linting error

--- app/(dashboard)/tasks/page.tsx
+++ app/(dashboard)/tasks/page.tsx
@@ -174,7 +174,7 @@ export default async function TasksPage({
         </div>
 
         {filteredTasks.length > 0 ? (
-          <TaskList tasks={sortedTasks} showProject={true} />
+          <TaskList initialTasks={sortedTasks}/>
         ) : (
           <div className="p-12 text-center">
             <div className="text-gray-400 text-6xl mb-4">📝</div>
