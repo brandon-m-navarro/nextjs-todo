3e8f8ee1a463ddabf5cb9d023d1e34853e5a7126
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-10-13 13:23:09 -0400

    Commenting out ProjectPage to help debug obscure minified react error

--- app/(dashboard)/projects/[projectId]/page.tsx
+++ app/(dashboard)/projects/[projectId]/page.tsx
@@ -1,13 +1,15 @@
-import ProjectPageComponent from "@/app/components/projects/ProjectPageComponent";
+// import ProjectPageComponent from "@/app/components/projects/ProjectPageComponent";
 
-interface ProjectPageProps {
-  params: Promise<{
-    projectId: string;
-  }>;
-}
+// interface ProjectPageProps {
+//   params: Promise<{
+//     projectId: string;
+//   }>;
+// }
 
-export default async function ProjectPage({ params }: ProjectPageProps) {
+// export default async function ProjectPage({ params }: ProjectPageProps) {
+  export default function ProjectPage() {
+
   // Dynamic routes must be awaited
-  const { projectId } = await params;
-  return <ProjectPageComponent projectId={projectId}></ProjectPageComponent>;
+  // const { projectId } = await params;
+  // return <ProjectPageComponent projectId={projectId}></ProjectPageComponent>;
 }
