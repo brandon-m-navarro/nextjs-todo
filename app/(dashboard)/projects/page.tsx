5314d02536b0d1c629443f2ea06ed623af6aad79
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-10-15 14:38:39 -0400

    Missed increasing padding-bottom on projects/page

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -4,7 +4,7 @@ import ProjectChooser from "@/app/components/projects/ProjectChooser";
 
 export default function ProjectsPage() {
   return (
-    <div className="pb-12 sm:p-6 md:p-8 max-w-6xl mx-auto">
+    <div className="pb-15 sm:p-6 md:p-8 max-w-6xl mx-auto">
       <BackButton />
       <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 mb-6 sm:mb-8">
         <div className="flex-1">
