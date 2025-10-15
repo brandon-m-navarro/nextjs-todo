67ad7f3c4fcdcc669ff4a456404232a1d42bcafa
brandon-m-navarro <brandon.m.navarro@gmail.com>
2025-10-15 14:52:06 -0400

    Removed Head tag as it was preventing scrolling to show back button? added CSS to prevent zooming on small screens when focusing an input

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes"
-        />
-      </head>
       <body
         className={`antialiased min-h-screen flex-1 p-6 bg-white! select-none`}
       >
