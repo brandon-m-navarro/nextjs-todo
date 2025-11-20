2efd0818865dc7b2dc39f1b984f5ab706c5fa71f
Brandon <brandon.m.navarro@gmail.com>
2025-11-20 18:59:49 -0500

    tweaking route

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,21 +1,21 @@
 import { authClient } from "@/lib/auth-client";
 
 export async function GET(req: Request) {
-  // const url = new URL(req.url);
-  // const code = url.searchParams.get("code");
+  const url = new URL(req.url);
+  const code = url.searchParams.get("code");
 
   // if (!code) {
   //   return new Response("Missing code", { status: 400 });
   // }
 
   // This calls BetterAuth’s /oauth2/token endpoint
-  //   const { data, error } = await authClient.oauth2.token({
-  //     grant_type: "authorization_code",
-  //     code,
-  //     redirect_uri: "https://todo.bnav.dev/auth/callback",
-  //     client_id: process.env.TODO_CLIENT_ID as string,
-  //     client_secret: process.env.TODO_CLIENT_SECRET as string,
-  //   });
+    const { data, error } = await authClient.oauth2.token({
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: "https://todo.bnav.dev/auth/callback",
+      client_id: process.env.TODO_CLIENT_ID as string,
+      client_secret: process.env.TODO_CLIENT_SECRET as string,
+    });
 
   // const user = await authClient.oauth2.userinfo({
   //   //   accessToken: data?.access_token as string,
@@ -27,7 +27,7 @@ export async function GET(req: Request) {
     "https://login.bnav.dev/api/auth/oauth2/userinfo",
     {
       headers: {
-        Authorization: "Bearer ACCESS_TOKEN",
+        Authorization: `Bearer ${data?.access_token}`,
       },
     }
   );
@@ -39,5 +39,5 @@ export async function GET(req: Request) {
   //     return new Response(error.message, { status: 400 });
   //   }
 
-  return Response.redirect("https://todo.bnav.dev/dashboard");
+  return Response.redirect("https://todo.bnav.dev/projects");
 }
