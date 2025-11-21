c8d1f28f0f48948877c0335a46d70504630d6388
Brandon <brandon.m.navarro@gmail.com>
2025-11-20 19:33:28 -0500

    tweaking route

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,39 +1,44 @@
-import { authClient } from "@/lib/auth-client";
+// import { authClient } from "@/lib/auth-client";
+import { cookies } from 'next/headers';
 
 export async function GET(req: Request) {
-  const url = new URL(req.url);
-  const code = url.searchParams.get("code");
+  // const url = new URL(req.url);
+  // const code = url.searchParams.get("code");
 
   // if (!code) {
   //   return new Response("Missing code", { status: 400 });
   // }
 
   // This calls BetterAuth’s /oauth2/token endpoint
-  const { data, error } = await authClient.oauth2.token({
-    grant_type: "authorization_code",
-    code,
-    redirect_uri: "https://todo.bnav.dev/auth/callback",
-    client_id: process.env.TODO_CLIENT_ID as string,
-    client_secret: process.env.TODO_CLIENT_SECRET as string,
-  });
+  //   const { data, error } = await authClient.oauth2.token({
+  //     grant_type: "authorization_code",
+  //     code,
+  //     redirect_uri: "https://todo.bnav.dev/auth/callback",
+  //     client_id: process.env.TODO_CLIENT_ID as string,
+  //     client_secret: process.env.TODO_CLIENT_SECRET as string,
+  //   });
 
-  console.log("OAuth2 Token Response:", data, error);
-
-  if (error || !data) {
-    return new Response("Failed to exchange code for token", { status: 500 });
-  }
-
-  // Optionally, fetch user info using the access token
+  // const user = await authClient.oauth2.userinfo({
+  //   //   accessToken: data?.access_token as string,
+  // });
+  // console.log("TESTING");
+  // console.log("OAuth2 User Info:", user);
+  const atoken = (await cookies()).get('__Secure-better-auth.session_token')?.value;
   const response = await fetch(
     "https://login.bnav.dev/api/auth/oauth2/userinfo",
     {
       headers: {
-        Authorization: `Bearer ${data?.access_token}`,
+        Authorization: `Bearer ${atoken}`,
       },
     }
   );
   const userInfo = await response.json();
   console.log("OAuth2 User Info:", userInfo);
+
+  //   // Handle error
+  //   if (error) {
+  //     return new Response(error.message, { status: 400 });
+  //   }
 
   return Response.redirect("https://todo.bnav.dev/projects");
 }
