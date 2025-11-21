d7cf9f64f7e2201b3b74f5e8882587b3bab2ee32
Brandon <brandon.m.navarro@gmail.com>
2025-11-21 06:43:53 -0500

    missing api/ in redirect URI

--- app/api/auth/sso/login/route.ts
+++ app/api/auth/sso/login/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
   authorizeURL.searchParams.set("client_id", process.env.TODO_CLIENT_ID!);
   authorizeURL.searchParams.set(
     "redirect_uri",
-    "https://todo.bnav.dev/auth/callback"
+    "https://todo.bnav.dev/api/auth/callback"
   );
   authorizeURL.searchParams.set("response_type", "code");
   authorizeURL.searchParams.set("scope", "openid profile email");
@@ -18,3 +18,7 @@ export async function GET() {
 
   return NextResponse.redirect(authorizeURL.toString(), 302);
 }
+
+//https://todo.bnav.dev/auth/callback
+// ?code=t0KaRc1jUr1iTRMI3oP4Mxy4DopUW0DE
+// &state=9e447ee2-1023-496d-a9d8-9c489eb59992
